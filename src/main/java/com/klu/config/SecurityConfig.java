74c98b9be497f58fe7d89c240157e65689615b15
VEERAGANI SAMPATH KUMAR <vsampathkumar0117@gmail.com>
2026-04-01 14:15:36 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/klu/config/SecurityConfig.java
+++ src/main/java/com/klu/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "ecommercebackend-production-feda.up.railway.app"
+            "https://ecommercefrontend-production-20aa.up.railway.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
